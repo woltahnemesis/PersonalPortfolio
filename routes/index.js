04373620592c5bd96c9932134be396e589c2d011
Rey Walter Magbanua <alucardmagbanua@gmail.com>
2020-07-06 21:56:32 -0400

    Designed the web with simplicity. Added a working contact form. Added wireframe. Finished front and back-end.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-  'use strict';
+//File Name: PersonalPortfolio
+//Author's Name: Walter Magbanua
+//Website Name: Web Portfolio
+//Description: This website is a web portfolio and has 5 different pages (Home, About Me, Contact Me, Services, & Projects).
+//Reference: 
+//Contact Form Solution By: Codtex https://stackoverflow.com/questions/45478293/username-and-password-not-accepted-when-using-nodemailer
+//How To Get Post Data By: The Net Ninja https://www.youtube.com/watch?v=rin7gb9kdpk
+//The Nodemailer Module By: W3Schools https://www.w3schools.com/nodejs/nodejs_email.asp
+
+'use strict';
 var express = require('express');
 var router = express.Router();
-var nodemailer = require('nodemailer');
-var bodyParser = require('body-parser');
 
+// Package to be able to send email
+var nodemailer = require('nodemailer');
+
+// These are needed when taking data from the form
+var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 /* GET home page. */
@@ -32,13 +44,20 @@ router.get('/service', function (req, res) {
 });
 
 /* POST method for Contact Form */
+// urlencodedParser must be in the parameter to get form data
 router.post('/contact', urlencodedParser, function (req, res) {
+
+    // To show inputs
     console.log(req.body);
+
     res.render('contact');
+
+    // Data taken from the inputs
     var email = req.body.fromEmail;
     var password = req.body.password;
     var message = req.body.message;
-    
+
+    // To sign in gmail account
     var transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
@@ -47,6 +66,7 @@ router.post('/contact', urlencodedParser, function (req, res) {
         }
     });
 
+    // To send message via gmail
     var mailOptions = {
         from: email,
         to: 'alucardmagbanua@gmail.com',
@@ -54,6 +74,7 @@ router.post('/contact', urlencodedParser, function (req, res) {
         text: message
     };
 
+    // To find errors
     transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
             console.log(error);
